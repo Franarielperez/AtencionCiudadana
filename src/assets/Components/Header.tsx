99397f7c5ca9b02cd6d0b1034a60ff61a077621a
Franariperez <Franariel21@hotmail.com>
2025-07-02 01:39:27 -0300

    Primer repositorio

---
+++ src/assets/Components/Header.tsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="header">
+        <h1 className="text-red-700">Hola</h1>
+      </div>
+    );
+  }
+}
+
+export default Header;
